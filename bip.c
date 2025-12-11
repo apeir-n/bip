f4f892862f0013b7377350dc18c4fdcb0c903186
apeir-n <brshellito@gmail.com>
2025-12-10 22:36:06 -0500

    [ fuck yeah ][ wed_251210 ][ 10:36pm ][ apeir_n ]

--- bip.c
+++ bip.c
@@ -21,6 +21,11 @@ enum Operand {
     VAR_C /* const */
 };
 
+typedef enum {
+    EXPR_OP,
+    EXPR_VAL
+} ExprKind;
+
 typedef struct {
     unsigned char r, g, b;
 } Pixel;
@@ -31,12 +36,143 @@ typedef struct {
     Pixel **pix;
 } Image;
 
-typedef struct {
-    enum Operator op;
-    enum Operand l;
-    enum Operand r;
-    int constant;
+typedef struct Expr {
+    ExprKind kind;
+    union {
+        struct {
+            enum Operator op;
+            struct Expr *l;
+            struct Expr *r;
+        } op_expr;
+        enum Operand val_expr;
+    };
 } Expr;
+
+int depth_param(int depth, int random) {
+
+    /* 
+     * depth = 1       only returns 1 leaf:            a
+     * depth = 2       returns 1 branch:               (a ? b)
+     * depth = 3       returns 3 branches, 4 leaves:   ((a ? b) ? (c ? d))
+     * depth = 4       returns 7 branches, 8 leaves:   (((a ? b) ? (c ? d)) ? ((e ? f) ? (g ? h)))
+     * 
+     * here the depth is minimized at 2, and randomness is optionally added to increase the number
+     * iterations (branches). random is maxed at 4, and if it's zero, then the same number of branches
+     * are made each time the expression is generated.
+     */
+
+    if (depth < 2) depth = 2;
+    if (random > 4) random = 4;
+    /* return depth + (random > 0 ? rand() % random : 0); */
+    return depth + (rand() % random);
+}
+
+Expr *make_expr(int depth) {
+    Expr *e = malloc(sizeof(Expr));
+
+    if (depth <= 1) {
+        /* base case: make a leaf */
+        e->kind = EXPR_VAL;
+        e->val_expr = rand() % 3;
+        return e;
+    }
+
+    /* recursive case: make a branch */
+    e->kind = EXPR_OP;
+    e->op_expr.op = rand() % 10;
+    e->op_expr.l = make_expr(depth - 1);
+    e->op_expr.r = make_expr(depth - 1);
+    return e;
+}
+
+int eval_expr(Expr *e, int x, int y, int c) {
+    if (e->kind == EXPR_VAL) {
+        switch (e->val_expr) {
+            case VAR_X: return x;
+            case VAR_Y: return y;
+            case VAR_C: return c;
+            default: return 0;
+        }
+    }
+
+    int a = eval_expr(e->op_expr.l, x, y, c);
+    int b = eval_expr(e->op_expr.r, x, y, c);
+
+    switch (e->op_expr.op) {
+        case OP_ADD: return a + b;
+        case OP_SUB: return a - b;
+        case OP_MUL: return a * b;
+        case OP_DIV: return b == 0 ? 0 : a / b;
+        case OP_MOD: return b == 0 ? 0 : a % b;
+        case OP_AND: return a & b;
+        case OP_OOR: return a | b;
+        case OP_XOR: return a ^ b;
+        case OP_SHL: return a << (b % 8);
+        case OP_SHR: return a >> (b % 8);
+        default: return 0;
+    }
+}
+
+void print_expr(Expr *e) {
+    if (e->kind == EXPR_VAL) {
+        switch (e->val_expr) {
+            case VAR_X: printf("x"); break;
+            case VAR_Y: printf("y"); break;
+            case VAR_C: printf("c"); break;
+        }
+        return;
+    }
+    
+    printf("(");
+    print_expr(e->op_expr.l);
+
+    switch (e->op_expr.op) {
+        case OP_ADD: printf(" + ");  break;
+        case OP_SUB: printf(" - ");  break;
+        case OP_MUL: printf(" * ");  break;
+        case OP_DIV: printf(" / ");  break;
+        case OP_MOD: printf(" %% "); break;
+        case OP_AND: printf(" & ");  break;
+        case OP_OOR: printf(" | ");  break;
+        case OP_XOR: printf(" ^ ");  break;
+        case OP_SHL: printf(" << "); break;
+        case OP_SHR: printf(" >> "); break;
+    }
+
+    print_expr(e->op_expr.r);
+    printf(")");
+}
+
+void free_expr(Expr *e) {
+    if (e->kind == EXPR_OP) {
+        free_expr(e->op_expr.l);
+        free_expr(e->op_expr.r);
+    }
+    free(e);
+}
+
+void palloc(Image *img) {
+    img->pix = malloc(img->h * sizeof(Pixel *));
+    if (!img->pix) {
+        fprintf(stderr, "couldn't malloc rows 🙈💩💥\n");
+        exit(1);
+    }
+    for (int i = 0; i < img->h; i += 1) {
+        img->pix[i] = malloc(img->w * sizeof(Pixel));
+        if (!img->pix[i]) {
+            fprintf(stderr, "couldn't malloc row %d 🙈💩💥\n", i);
+            exit(1);
+        }
+    }
+
+}
+
+void pfree(Image *img) {
+    for (int i = 0; i < img->h; i += 1) {
+        free(img->pix[i]);
+    }
+    free(img->pix);
+}
 
 void save(Image *img) {
     FILE *f = fopen(img->name, "wb");
@@ -90,84 +226,14 @@ void save(Image *img) {
     fclose(f);
 }
 
-int getop(enum Operand which, int x, int y, int c) {
-    switch (which) {
-        case VAR_X: return x;
-        case VAR_Y: return y;
-        case VAR_C: return c;
-        default: return 0;
-    }
-}
-
-int eval(Expr e, int x, int y) {
-    int a = getop(e.l, x, y, e.constant);
-    int b = getop(e.r, x, y, e.constant);
-
-    switch (e.op) {
-        case OP_ADD: return a + b;
-        case OP_SUB: return a - b;
-        case OP_MUL: return a * b;
-        case OP_DIV: return b == 0 ? 0 : a / b;
-        case OP_MOD: return b == 0 ? 0 : a % b;
-        case OP_AND: return a & b;
-        case OP_OOR: return a | b;
-        case OP_XOR: return a ^ b;
-        case OP_SHL: return a << (b % 8);
-        case OP_SHR: return a >> (b % 8);
-        default: return 0;
-    }
-}
-
-Expr randexpr() {
-    Expr e;
-    e.op = rand() % 10;
-    e.l = rand() % 3;
-    e.r = rand() % 3;
-    e.constant = rand() % 100;
-    return e;
-}
-
-const char* opname(int op) {
-    switch (op) {
-        case OP_ADD: return "ADD";
-        case OP_SUB: return "SUB";
-        case OP_MUL: return "MUL";
-        case OP_DIV: return "DIV";
-        case OP_MOD: return "MOD";
-        case OP_AND: return "AND";
-        case OP_OOR: return "OOR";
-        case OP_XOR: return "XOR";
-        case OP_SHL: return "SHL";
-        case OP_SHR: return "SHR";
-        default: return "???";
-    }
-}
-
-const char* varname(int var) {
-    switch (var) {
-        case VAR_X: return "X";
-        case VAR_Y: return "Y";
-        case VAR_C: return "C";
-        default: return "???";
-    }
-}
-
-void printexpr(Expr e) {
-    const char *op_str[] = { "+", "-", "*", "/", "%", "&", "|", "^", "<<", ">>" };
-    const char *var_str[] = { "x", "y", "c" };
-
-    printf("evaluating: (%s %s %s), where c = %d\n",
-            var_str[e.l], op_str[e.op], var_str[e.r], e.constant);
-}
-
 void write(Image *img) {
-    Expr ptrn = randexpr();
-    printexpr(ptrn);
+    Expr *ptrn = make_expr(depth_param(4, 2));
+    int c = rand() % 30;
 
     for (int y = 0; y < img->h; y += 1) {
         for (int x = 0; x < img->w; x += 1) {
-            int val = eval(ptrn, x, y);
-            int thresh = 12;
+            int val = eval_expr(ptrn, x, y, c);
+            int thresh = 5;
 
             if (val % 256 > thresh) {
                 img->pix[y][x] = (Pixel){0xff, 0xff, 0xff};
@@ -176,29 +242,12 @@ void write(Image *img) {
             }
         }
     }
-}
 
-void palloc(Image *img) {
-    img->pix = malloc(img->h * sizeof(Pixel *));
-    if (!img->pix) {
-        fprintf(stderr, "couldn't malloc rows 🙈💩💥\n");
-        exit(1);
-    }
-    for (int i = 0; i < img->h; i += 1) {
-        img->pix[i] = malloc(img->w * sizeof(Pixel));
-        if (!img->pix[i]) {
-            fprintf(stderr, "couldn't malloc row %d 🙈💩💥\n", i);
-            exit(1);
-        }
-    }
-
-}
-
-void pfree(Image *img) {
-    for (int i = 0; i < img->h; i += 1) {
-        free(img->pix[i]);
-    }
-    free(img->pix);
+    printf("expression: ");
+    print_expr(ptrn);
+    printf("\n");
+    printf("where c = %d\n", c);
+    free_expr(ptrn);
 }
 
 int main() {
--- etc/notes.md
+++ etc/notes.md
@@ -1,5 +1,18 @@
 # notes to self
 
+ideas
+- make it modular?
+    - expression generator as its own binary that writes to stdout
+    - pipe the output into the image generator
+    - make script to do that and open with an image viewer
+        ```sh
+        #!/bin/sh
+
+        expressia | bip && xdg-open *.bmp
+        ```
+- i'd probably have to rewrite the whole thing to be able to parse the literal expressions
+    - since i'm using internal enums for all the components and converting them to * / + - etc symbols later in order to print
+
 ## parameters for cli later
 - default is random, but user can input expression that will be passed to `int val` in `write()`
 - maybe also `int thresh`
